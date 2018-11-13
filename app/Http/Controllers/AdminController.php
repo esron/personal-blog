e3153d84489f40a30a8319e12e8052a78d37d62b
esron <esron.dtamar@gmail.com>
2018-11-13 16:01:23 -0300

    add product edition

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -152,11 +152,8 @@ class AdminController extends Controller
         $product->price = $request->price;
 
         $thumbnail = $request->file('thumbnail');
-
         $fileName = $thumbnail->getClientOriginalName();
-
         $thumbnail->move('product-images', $fileName);
-
         $product->thumbnail = 'product-images/'.$fileName;
 
         $product->save();
@@ -164,14 +161,38 @@ class AdminController extends Controller
         return back();
     }
 
-    public function editProduct()
+    public function editProduct($id)
     {
+        $product = Product::findOrFail($id);
 
+        return view('admin.editProduct', compact('product'));
     }
 
     public function editProductPost(Request $request, $id)
     {
+        $this->validate($request, [
+            'title' => 'required|string',
+            'thumbnail' => 'file',
+            'description' => 'required',
+            'price' => 'required|regex:/^[0-9]+(\.[0-9][0-9]?)?$/'
+        ]);
 
+        $product = Product::findOrFail($id);
+
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->price = $request->price;
+
+        if ($request->hasFile('thumbnail')) {
+            $thumbnail = $request->file('thumbnail');
+            $fileName = $thumbnail->getClientOriginalName();
+            $thumbnail->move('product-images', $fileName);
+            $product->thumbnail = 'product-images/'.$fileName;
+        }
+
+        $product->save();
+
+        return back();
     }
 
     public function deleteProductPost(Request $request, $id)
